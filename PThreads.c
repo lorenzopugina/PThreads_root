b94b4b52c461d3739a978273514b7e9503c6d508
lorenzopugina <lorenzopugina10@gmail.com>
2024-10-11 15:26:45 -0300

    Arquivos numeros aleatorios feitos

--- PThreads.c
+++ PThreads.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void)
-{
-    printf("Main do arquivo\n");
+int main() {
+    
+    int quantidade = 1000;
+    
+    srand(time(0));
+
+    FILE *arquivo = fopen("arq5.txt", "w+");
+    
+   
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1; 
+    }
+
+    int espaco = 0;
+    
+    for(int i = 0; i < quantidade; i++) {
+        espaco++;
+        int numero_aleatorio = rand() % 1001;  
+        fprintf(arquivo, "%d ", numero_aleatorio); 
+
+        if (espaco == 25){
+           fprintf(arquivo, "\n");
+           espaco = 0;
+        }
+        
+    }
+
+    fclose(arquivo);
+
     return 0;
 }
