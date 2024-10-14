a626b358e658fc0ad217868a5e6188ed6888e523
lorenzopugina <lorenzopugina10@gmail.com>
2024-10-14 15:32:16 -0300

    Começo

---
+++ FazNumAleatorio.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    
+    int quantidade = 1000;
+    
+    srand(time(0));
+
+    FILE *arquivo = fopen("arq5.txt", "w+");
+    
+   
+    if (arquivo == NULL) {
+        printf("Num abriu\n");
+        return 1; 
+    }
+
+    int quebraLinha = 0;
+    
+    for(int i = 0; i < quantidade; i++) {
+        quebraLinha++;
+        int numero_aleatorio = rand() % 1001;  
+        fprintf(arquivo, "%d ", numero_aleatorio); 
+
+        if (quebraLinha == 25){
+           fprintf(arquivo, "\n");
+           quebraLinha = 0;
+        }
+        
+    }
+
+    fclose(arquivo);
+
+    return 0;
+}
--- PThreads.c
+++ PThreads.c
@@ -1,36 +1,9 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int main() {
-    
-    int quantidade = 1000;
-    
-    srand(time(0));
-
-    FILE *arquivo = fopen("arq5.txt", "w+");
-    
-   
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return 1; 
-    }
-
-    int espaco = 0;
-    
-    for(int i = 0; i < quantidade; i++) {
-        espaco++;
-        int numero_aleatorio = rand() % 1001;  
-        fprintf(arquivo, "%d ", numero_aleatorio); 
-
-        if (espaco == 25){
-           fprintf(arquivo, "\n");
-           espaco = 0;
-        }
-        
-    }
-
-    fclose(arquivo);
-
+int main(int argc, char const *argv[])
+{
+    /* code */
     return 0;
 }
