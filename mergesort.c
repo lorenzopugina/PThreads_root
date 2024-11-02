27458e1eca23e56d828a67f14d34c190e49cafff
Lorenzo Pugina <lorenzopugina10@gmail.com>
2024-11-02 15:31:32 -0300

    Refatoração

--- mergesort.c
+++ mergesort.c
@@ -17,63 +17,47 @@ typedef struct {
     int Indicecomeco;
     int Indicefinal;
     int* vetor;
-    sem_t* semaforoMerge;
 } parametrosMergesort;
 
 void* leitores(void* args);
-void redimensionar(int** vetor, int* capacidade); 
-void intercala(int Indicecomeco, int meio, int Indicefinal, int vetor[]);
+void intercala(int Indicecomeco, int meio, int Indicefinal, int* vetor);
 void* criaMerges(void* args);
-void mergesort (int Indicecomeco, int Indicefinal, int vetor[]);
+void mergesort (int Indicecomeco, int Indicefinal, int* vetor);
 void imprimeSaida(int* vetor, int tamanho, char* arquivo);
 
 int maxThreads;
-int contador = 0;
+int contador;
 
 int main(int argc, char const* argv[]){
 
+    if (argc == 1){
+        return 0;
+    }
+    
     maxThreads = atoi(argv[1]); // ascii to integer
     contador = maxThreads;
 
-    pthread_t* threads = (pthread_t*)malloc(maxThreads * sizeof(pthread_t)); // Aloca um vetor de threads   
-    if (threads == NULL){
-        printf("Alocação para threads falhou\n");
-        return 1;
-    }
-    
     sem_t semaforoLeitura;         
     sem_init(&semaforoLeitura, 0, 1);
 
-    int tamanho = 20 * (argc - 3);
+    int tamanho = 20 * (argc - 4);
     int* vetorPrincipal = (int*)malloc(tamanho * sizeof(int)); // Aloca um vetor inicial de 20 elementos para cada arquivo a ler
-    if (vetorPrincipal == NULL){
-        printf("Alocação inicial do vetor falhou\n");
-        return 1;
-    }
+    
+    pthread_t* threads = (pthread_t*)malloc(maxThreads * sizeof(pthread_t)); // Aloca um vetor de threads 
+    parametrosLeitura* leitura = malloc(maxThreads * sizeof(parametrosLeitura));  
 
     int numElementos = 0;
- 
-     for (int j = 2; j < argc-2; j++){ // para cada arquivo a ser lido
 
+     for (int j = 2; j < argc-2; j++){ // para cada arquivo a ser lido
         FILE* arquivo = fopen(argv[j], "r");  // arquivo da vez
-        if (arquivo == NULL){
-             printf("Não abriu o arquivo para leitura");
-             return 1;
-        }
 
         for (int i = 0; i < maxThreads; i++) {
-            parametrosLeitura* args = (parametrosLeitura*)malloc(sizeof(parametrosLeitura)); 
-            if (args == NULL){
-                printf("Alocação para parâmetros de leitura falhou\n");
-                return 1;
-            }
-            
-            args->arquivo = arquivo; 
-            args->vetor = &vetorPrincipal;
-            args->numElementos = &numElementos;
-            args->tamanho = &tamanho;
-            args->semaforoLeitura = &semaforoLeitura;
-            pthread_create(&threads[i], NULL, leitores, args); 
+            leitura[i].arquivo = arquivo; 
+            leitura[i].vetor = &vetorPrincipal;
+            leitura[i].numElementos = &numElementos;
+            leitura[i].tamanho = &tamanho;
+            leitura[i].semaforoLeitura = &semaforoLeitura;
+            pthread_create(&threads[i], NULL, leitores, &leitura[i]); 
         }
 
         // Aguardar as threads antes de trocar o arquivo
@@ -83,30 +67,37 @@ int main(int argc, char const* argv[]){
 
         fclose(arquivo);
     }
+    free(threads);
+    free(leitura);
+    
+    pthread_t* threadsMerge = (pthread_t*)malloc(maxThreads * sizeof(pthread_t)); // Aloca um vetor de threads 
+    parametrosMergesort* merge = malloc(maxThreads * sizeof(parametrosMergesort));
+    
+    int elementosPorThread = numElementos / maxThreads;
+    int resto = numElementos % maxThreads;
+    int indiceComeço = 0;
 
-    sem_t semaforoMerge;
-    sem_init(&semaforoMerge, 0, 1);
+    for (int i = 0; i < maxThreads; i++) {
+        merge[i].vetor = vetorPrincipal; 
+        merge[i].Indicefinal = indiceComeço + elementosPorThread + (i < resto ? 1 : 0); // Distribui o resto
+        merge[i].Indicecomeco = indiceComeço;
 
-    // Criação da primeira thread merge
-    parametrosMergesort args = {0, numElementos, vetorPrincipal, &semaforoMerge}; 
-    pthread_t threadInical;
+        pthread_create(&threadsMerge[i], NULL, criaMerges, &merge[i]);
+        indiceComeço =  merge[i].Indicefinal;
+    }
 
-    clock_t inicio = clock(); 
+    for (int i = 0; i < maxThreads; i++) {
+        pthread_join(threadsMerge[i], NULL);
+    }
 
-    pthread_create(&threadInical, NULL, criaMerges, &args); 
-
-    // Espera a thread principal terminar 
-    pthread_join(threadInical, NULL);
-    sem_destroy(&semaforoMerge); 
-
-    clock_t fim = clock();
-    double tempoExecucao = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("Tempo total de execução: %f segundos\n", tempoExecucao); 
+    // mergesort final
+    mergesort(0, numElementos, vetorPrincipal);  
 
     imprimeSaida(vetorPrincipal, numElementos, strdup(argv[argc-1]));
 
     free(vetorPrincipal);
-    free(threads);
+    free(merge);
+    free(threadsMerge);
 
     return 0;
 }
@@ -120,101 +111,41 @@ void* leitores(void* args){
         
         // Verifica se precisa redimensionar o vetor
         if (*param->numElementos >= *param->tamanho) {
-            redimensionar(param->vetor, param->tamanho);
+            *param->vetor = realloc(*param->vetor, (*param->tamanho += 50) * sizeof(int)); //------------------------------------ CONFUSO?
         }
 
         (*param->vetor)[(*param->numElementos)++] = num;          
        sem_post(param->semaforoLeitura); // up
     }
-    
-    free(param); // Necessário pois foi alocada memoria
     pthread_exit(NULL);
 }
 
-void redimensionar(int** vetor, int* capacidade){
-
-    *capacidade += 50; 
-
-    int* vetorAux = realloc(*vetor, (*capacidade) * sizeof(int)); 
-    if (vetorAux == NULL) {
-        printf("Realocação falhou\n");
-        return; 
-    }
-
-     *vetor = vetorAux; 
-}
-
-void* criaMerges(void* args){
-    clock_t inicio = clock(); 
+void* criaMerges(void* args) {
+    clock_t inicio = clock(); // aqui -------------------------------
 
     parametrosMergesort* param = (parametrosMergesort*) args;
     int Indicecomeco = param->Indicecomeco;
     int Indicefinal = param->Indicefinal;
-    int* vetor = param->vetor;
 
-    // Enquanto o vetor for maior que 1, será divido ao meio
-    if (Indicecomeco < Indicefinal - 1) { 
-        int meio = (Indicecomeco + Indicefinal) / 2; 
-
-        // Criação de threads para cada metade do vetor
-        parametrosMergesort arg1 = {Indicecomeco, meio, vetor, param->semaforoMerge};
-        parametrosMergesort arg2 = {meio, Indicefinal, vetor, param->semaforoMerge};
-        pthread_t thread1, thread2; 
-        int criouThread1 = 0, criouThread2 = 0; 
-
-        sem_wait(param->semaforoMerge); // down
-
-        if (maxThreads > 1) { // Cada thread recebe metade do vetor
-            maxThreads--;
-            sem_post(param->semaforoMerge); // up
-
-            pthread_create(&thread1, NULL, criaMerges, &arg1);
-            criouThread1 = 1;
-        } else {
-            sem_post(param->semaforoMerge); // up
-            mergesort(Indicecomeco, meio, vetor); // Se já estiver no limite de threads, continua com a recursividade
-        }
-
-        sem_wait(param->semaforoMerge); // down
-         // O mesmo processo para a segunda metade do vetor
-        if (maxThreads > 1) { 
-            maxThreads--;
-             sem_post(param->semaforoMerge); // up
-
-            pthread_create(&thread2, NULL, criaMerges, &arg2);
-            criouThread2 = 1;
-        } else {
-             sem_post(param->semaforoMerge); // up
-            mergesort(meio, Indicefinal, vetor);
-        }
-
-        // Espera as threads terminarem de dividir o vetor antes de ir para a ordenação
-        if (criouThread1) {
-            pthread_join(thread1, NULL);
-        }
-        if (criouThread2) {
-            pthread_join(thread2, NULL);
-        }
-
-        // Ordena e mescla as metades
-        intercala(Indicecomeco, meio, Indicefinal, vetor);
+    if (Indicecomeco < Indicefinal - 1) {
+        int meio = (Indicecomeco + Indicefinal) / 2;
+        mergesort(Indicecomeco, meio, param->vetor); 
+        mergesort(meio, Indicefinal, param->vetor);  
+ 
+        intercala(Indicecomeco, meio, Indicefinal, param->vetor);
     }
-    
+
     clock_t fim = clock();
     double tempoExecucao = (double)(fim - inicio) / CLOCKS_PER_SEC;
-
-    sem_wait(param->semaforoMerge); // down
-    printf("Tempo de execução da thread %d: %f segundos\n", contador--, tempoExecucao);
-    sem_post(param->semaforoMerge); // up
+    printf("Tempo total de execução da thread %d foi de: %f segundos\n", contador--, tempoExecucao);
 
     pthread_exit(NULL);
 }
 
-
-void mergesort(int Indicecomeco, int Indicefinal, int vetor[]){
+void mergesort(int Indicecomeco, int Indicefinal, int* vetor){
 
     // Enquanto o vetor for maior que 1, será divido ao meio  
-    if (Indicecomeco < (Indicefinal-1)) {        
+    if (Indicecomeco < Indicefinal - 1) {        
       int meio = (Indicecomeco + Indicefinal)/2; 
       mergesort (Indicecomeco, meio, vetor);     
       mergesort (meio, Indicefinal, vetor);  
@@ -224,14 +155,9 @@ void mergesort(int Indicecomeco, int Indicefinal, int vetor[]){
     }
 }
 
-void intercala(int Indicecomeco, int meio, int Indicefinal, int vetor[]){
+void intercala(int Indicecomeco, int meio, int Indicefinal, int* vetor){
 
     int* vetorAux = (int*)malloc((Indicefinal-Indicecomeco) * sizeof (int));  
-    if (vetorAux == NULL) {
-        printf("Alocação para vetor auxiliar falhou\n");
-        return; 
-    }
-    
     int comeco = Indicecomeco;
     int metade = meio;  
     int contador = 0;  
@@ -257,20 +183,13 @@ void intercala(int Indicecomeco, int meio, int Indicefinal, int vetor[]){
     for (comeco = Indicecomeco; comeco < Indicefinal; ++comeco){ 
         vetor[comeco] = vetorAux[comeco-Indicecomeco];  
     }  
-
     free(vetorAux);  
 }
 
 void imprimeSaida(int* vetor, int tamanho, char* arquivo){
-
     int quebraLinha = 0;
 
     FILE* saida = fopen(arquivo, "w+");
-    if (saida == NULL){
-        printf("Não abriu o arquivo para escrita");
-        free(arquivo);
-        return;
-    }
 
     for (int i = 0; i < tamanho; i++) { 
         quebraLinha++;
@@ -281,7 +200,6 @@ void imprimeSaida(int* vetor, int tamanho, char* arquivo){
             quebraLinha = 0;
         }
     }
-
     fclose(saida);
     free(arquivo); // Necessário devido ao strdup()
 }
